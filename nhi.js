8060a10795415b356e1c4885ff10d2669732019e
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2025-01-02 14:57:33 +1300

    update docs

--- nhi.js
+++ nhi.js
@@ -3,15 +3,6 @@
 const OLD_NHI_FORMAT = /^[A-HJ-NP-Z]{3}\d{4}$/;
 const NEW_NHI_FORMAT = /^[A-HJ-NP-Z]{3}\d{2}[A-HJ-NP-Z]{2}$/;
 
-/**
- * Checks a string against the New Zealand Ministry of Health NHI specification
- * defined by HISO 10046:2023 and the NHI validation routine
- *
- * @see {@link https://www.tewhatuora.govt.nz/publications/hiso-100462023-consumer-health-identity-standard/|HISO 10046:2023 Standard}
- *
- * @param {string} nhi a potential NHI number
- * @return {boolean} true if the given nhi value is a valid NHI number and false otherwise
- */
 export function isNhi(nhi) {
     nhi = nhi.toUpperCase();
     if (NEW_NHI_FORMAT.test(nhi)) {
@@ -28,11 +19,6 @@ export function isNhi(nhi) {
     }
 }
 
-
-/**
- * @param {number[]} codes
- * @returns {number}
- */
 function weightedCodes(codes) {
     return codes
         .slice(0, -1)
@@ -40,11 +26,6 @@ function weightedCodes(codes) {
         .reduce((acc, e) => acc + e, 0);
 }
 
-
-/**
- * @param {string} nhi
- * @returns {number[]}
- */
 function charCodes(nhi) {
     return [...nhi].map(c => {
         if (!isNaN(c)) {
